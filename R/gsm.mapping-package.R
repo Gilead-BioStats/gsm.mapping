a807dbc1b79219b48635611e84d0e94b055336f6
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-12-24 11:13:04 -0800

    First v.0.0.1 to see how this works (#10)
    * get on the board
    
    * bring in the 5 functions mentioned in issue
    
    * import the tests
    
    * tests should pass as is
    
    * get more steps going
    
    * please?
    
    * get action going
    
    * think it needs to be on main/master first
    
    * does this work
    
    * undo my mistakes
    
    * add all changes from latest gsm branch
    
    * cleanup a bit
    
    * update tests
    
    * does logger work
    
    * use a branch of gsm without mapping elements
    
    * rename and do stuff to main gsm branch
    
    * hopefully that works
    
    * what that do
    
    * does that fix it?
    
    * how does that affect ci/cd
    
    * please
    
    * grab the gsm dependency
    
    * there was so much more
    
    * use tidyr
    
    * does that fix it

---
+++ R/gsm.mapping-package.R
@@ -0,0 +1,30 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @import gsm
+#' @import purrr
+#' @import log4r
+#' @import tidyr
+#' @importFrom cli cli_abort
+#' @importFrom cli cli_alert
+#' @importFrom cli cli_alert_info
+#' @importFrom cli cli_alert_danger
+#' @importFrom cli cli_alert_success
+#' @importFrom cli cli_alert_warning
+#' @importFrom cli cli_text
+#' @importFrom cli cli_warn
+#' @importFrom dbplyr remote_con
+#' @importFrom dbplyr remote_name
+#' @importFrom DBI dbConnect
+#' @importFrom DBI dbExecute
+#' @importFrom DBI dbGetQuery
+#' @importFrom DBI dbDisconnect
+#' @importFrom DBI dbWriteTable
+#' @importFrom duckdb duckdb
+#' @importFrom glue glue
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_replace
+## usethis namespace: end
+NULL
