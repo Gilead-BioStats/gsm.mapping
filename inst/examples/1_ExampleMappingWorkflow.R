9d938309a47cdc575f9c6285ca0ebd0f2df81040
zdz2101 <zelos.zhu@atorusresearch.com>
2025-03-05 13:56:29 -0800

    push more sleek way to run example

--- inst/examples/1_ExampleMappingWorkflow.R
+++ inst/examples/1_ExampleMappingWorkflow.R
@@ -20,39 +20,8 @@ basic_sim <- gsm.datasim::generate_rawdata_for_single_study(
 )
 
 lSource <- basic_sim[[1]]
-
-lRaw <- list(
-  Raw_SUBJ = lSource$Raw_SUBJ,
-  Raw_AE = lSource$Raw_AE,
-  Raw_PD = lSource$Raw_PD %>%
-    rename(subjid = subjectenrollmentnumber),
-  Raw_LB = lSource$Raw_LB,
-  Raw_STUDCOMP = lSource$Raw_STUDCOMP %>%
-    select(subjid, compyn),
-  Raw_SDRGCOMP = lSource$Raw_SDRGCOMP,
-  Raw_DATACHG = lSource$Raw_DATACHG %>%
-    rename(subject_nsv = subjectname),
-  Raw_DATAENT = lSource$Raw_DATAENT %>%
-    rename(subject_nsv = subjectname),
-  Raw_QUERY = lSource$Raw_QUERY %>%
-    rename(subject_nsv = subjectname),
-  Raw_ENROLL = lSource$Raw_ENROLL,
-  Raw_SITE = lSource$Raw_SITE %>%
-    rename(studyid = protocol) %>%
-    rename(invid = pi_number) %>%
-    rename(InvestigatorFirstName = pi_first_name) %>%
-    rename(InvestigatorLastName = pi_last_name) %>%
-    rename(City = city) %>%
-    rename(State = state) %>%
-    rename(Country = country) %>%
-    rename(Status = site_status),
-  Raw_STUDY = lSource$Raw_STUDY %>%
-    rename(studyid = protocol_number) %>%
-    rename(Status = status),
-  Raw_PK = lSource$Raw_PK
-)
-
-
 # Step 1 - Create Mapped Data Layer - filter, aggregate and join raw data to create mapped data layer
-mappings_wf <- MakeWorkflowList(strName = core_mappings, strPath = "workflow/1_mappings", strPackage = "gsm.mapping")
+mappings_wf <- MakeWorkflowList(strNames = core_mappings, strPath = "workflow/1_mappings", strPackage = "gsm.mapping")
+mappings_spec <- CombineSpecs(mappings_wf)
+lRaw <- Ingest(lSource, mappings_spec)
 mapped <- RunWorkflows(mappings_wf, lRaw)
